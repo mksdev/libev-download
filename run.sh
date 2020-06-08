9625f373f4e0af25f9e116a981c8be47c86d0603
Michal Němec <michal.nemec.dev@gmail.com>
2020-06-08 22:52:33 +0200

    run.sh tab/spaces refactor

--- run.sh
+++ run.sh
@@ -8,43 +8,43 @@ PACKAGE_FOLDER=dist.schmorp.de/libev/Attic
 
 if [ -d "./$REPO_NAME" ] 
 then
-    cd $REPO_NAME
-    git pull
-    cd ..
+	cd $REPO_NAME
+	git pull
+	cd ..
 else
-    git clone $REPO_GIT $REPO_NAME
+	git clone $REPO_GIT $REPO_NAME
 fi
 
 for fi in $PACKAGE_FOLDER/libev-*.tar.gz; do
 	# remove path prefix
 	FILENAME=${fi//"${PACKAGE_FOLDER}\/"/""}
 	# get folder name (folder that is created from archive extraction)
-    FOLDER=${FILENAME//".tar.gz"/""}
-    #echo $FOLDER
-    VERSION=${FOLDER//"libev-"/""}
-    #echo $VERSION
-    echo "[$fi] processing $FILENAME, folder $FOLDER, version $VERSION"
+	FOLDER=${FILENAME//".tar.gz"/""}
+	#echo $FOLDER
+	VERSION=${FOLDER//"libev-"/""}
+	#echo $VERSION
+	echo "[$fi] processing $FILENAME, folder $FOLDER, version $VERSION"
 
-    cd $REPO_NAME
-    if git rev-parse v$VERSION >/dev/null 2>&1
+	cd $REPO_NAME
+	if git rev-parse v$VERSION >/dev/null 2>&1
 	then
-	    echo "skipping $FOLDER tag v$VERSION found"
-	    cd ..
-	    continue
+		echo "skipping $FOLDER tag v$VERSION found"
+		cd ..
+		continue
 	fi
 	# remove all files from repository
-    git rm -r *
-    cd ..
+	git rm -r *
+	cd ..
 
-    # extract package, this will extract package into current directory 
-    # (not directory in which package is located) 
-    tar -zxf $fi
-    cp ./$FOLDER/* ./$REPO_NAME
-    rm -rf ./$FOLDER
+	# extract package, this will extract package into current directory 
+	# (not directory in which package is located) 
+	tar -zxf $fi
+	cp ./$FOLDER/* ./$REPO_NAME
+	rm -rf ./$FOLDER
 
-    # commit new version files to the repo
-    cd $REPO_NAME
-    git add .
+	# commit new version files to the repo
+	cd $REPO_NAME
+	git add .
 	git commit -m "version $VERSION"
 	git tag v$VERSION
 	git push origin master
